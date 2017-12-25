b1f0372a4fa098eb89d54fa346a86cd693695bf6
stonelqf <876096545@qq.com>
2017-12-25 09:38:38 +0800

    目的：腾讯招聘案例爬虫

---
+++ Tencent/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TencentItem(scrapy.Item):
+    # define the fields for your item here like:
+    positionName = scrapy.Field()
+    positionategory = scrapy.Field()
+    peopleNumber = scrapy.Field()
+    loaction = scrapy.Field()
+    publishTime = scrapy.Field()
